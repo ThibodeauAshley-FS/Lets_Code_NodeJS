276f1bd4a00b203a37908c81378e634b3f65f335
ThibodeauAshley-FS <athibodeau1@student.fullsail.edu>
2023-07-03 17:21:01 -0400

    Added the 3 additional methods POST, PULL by ID, DELETE by ID

--- router/router.js
+++ router/router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// http://localhost:3000/exercise
+
+// GET http://localhost:3000/exercise
 router.get('/', (req,res, next) => {
     res.status(200).json({
         message: 'Successful - GET',
@@ -12,11 +13,50 @@ router.get('/', (req,res, next) => {
     });
 });
 
-// http://localhost:3000/exercise/34
+// GET BY ID http://localhost:3000/exercise/34
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
     res.status(200).json({
         message: 'Successful - GET by ID',
+        id: id,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+
+    });
+});
+
+// POST http://localhost:3000/exercise/POST
+router.post('/',  (req, res, next) => {
+    console.log('Request body >>>', req.body);
+    const { data } = req.body;
+    res.status(200).json({
+        message: 'Successful - POST',
+        data: data,
+    });
+});
+
+
+//PUT BY ID http://localhost:3000/exercise/34
+router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).json({
+        message: 'Successful - PUT by ID',
+        id: id,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+
+    });
+});
+
+//DELETE BY ID  http://localhost:3000/exercise/12
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).json({
+        message: 'Successful - DELETE by ID',
         id: id,
         metadata: {
             hostname: req.hostname,
